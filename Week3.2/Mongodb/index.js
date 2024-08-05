9be0d76dd0f9b3b34a96494fc03adc007ffb80f4
iamvbenz1 <iamvbenz49@github.com>
2024-08-05 19:59:59 +0530

    Basic Docker

---
+++ Week3.2/Mongodb/index.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose")
+
+mongoose.connect("mongodb+srv://iamvbenz:22092004@cluster0.mbiqubl.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",{
+    serverSelectionTimeoutMS: 30000
+  }).catch(err => {
+    console.error('Error connecting to MongoDB:', err.message);
+});
+  
+  
+
+const User = mongoose.model("Users", {name: String, email:String, password:String});
+
+const user = new User({
+    username: "harkirat@gmail.com",
+    password: "123",
+    name: "harkirat singh",
+})
+
+user.save()
+console.log("hello")
