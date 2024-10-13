15e984ccaa541860ef6aae05c167149d5b6651b7
Sham Vijay <benzwrites@gmail.com>
2024-10-13 22:38:51 +0530

    ORMs and Prisma

---
+++ week-13-prisma/src/05-get-post.ts
@@ -0,0 +1,28 @@
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient({log: ['info', 'query'],})
+
+async function main() {
+    const posts = await prisma.post.findMany({
+        include: {
+            author: {
+                select: {
+                    email: true,
+                    id:true
+                }
+            }
+        }
+    });
+    console.log(posts);
+};
+
+main()
+  .then(async () => {
+    console.log("done");
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
---
+++ week-13-prisma/src/09-update-posts.ts
@@ -0,0 +1,26 @@
+
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+async function main() {
+  await prisma.post.updateMany({
+    where: {
+        title:"title of post"
+    },
+    data: {
+        published:true
+    }
+  })
+}
+
+main()
+  .then(async () => {
+    console.log("done");
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
